9b05a4d5f36f96d4c0c13386ef1e8af6e4768e61
LABUDDAH <umar.usman2000toro@gmail.com>
2025-09-01 07:36:17 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for navigation (optional future feature)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
